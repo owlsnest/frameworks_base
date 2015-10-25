bc6e7b1d06e6b026df785e444294fb921d935af6
winga45 <winga45@gmail.com>
2015-10-25 12:18:07 +0000

    [BASE]:Recents Panel Clear all button base on cristanomatos from crdroid rom *show or hide clear all button *postion of the button

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3559,6 +3559,18 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
         /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+        */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
